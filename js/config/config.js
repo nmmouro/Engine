29bad897df94c708f82ef41a1c4596ac26918a99
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-19 20:29:38 -0300

    Create config.js

---
+++ js/config/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+
+    api: {
+        url: "https://script.google.com/macros/s/AKfycbwI3GFO4arC-VqyTxan0NFcdhKKWPBRFQTg78-EER8I8rLO08dm1foYvRL3J2SnhtpGsQ/exec"
+    },
+
+    ui: {
+        locale: "pt-BR"
+    }
+
+};
