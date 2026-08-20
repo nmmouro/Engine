d87cff44b83b699cb933c2db826284b73da187e0
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-19 21:08:09 -0300

    Update module.js

--- js/engine/module.js
+++ js/engine/module.js
@@ -1,7 +1,20 @@
-createModule({
-    entity,
+const form = createForm({
     schema,
-    container,
-    stateName,
-    options
+    container: formContainer,
+
+    onSubmit: async (dados, registroAtual) => {
+
+        if (registroAtual) {
+            await crud.atualizar(dados);
+        } else {
+            await crud.criar(dados);
+        }
+
+    },
+
+    onCancel: () => {
+
+        form.reset();
+
+    }
 });
