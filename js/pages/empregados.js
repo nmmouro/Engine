feb605e1eef4988000ec26430f31a807bf687a4f
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-20 22:00:54 -0300

    Create empregados.js for employee module setup

---
+++ js/pages/empregados.js
@@ -0,0 +1,32 @@
+import { createModule } from "../engine/module.js";
+
+import {
+    SCHEMA_EMPREGADOS
+} from "../schemas/empregadosschema.js";
+
+
+createModule({
+
+    entity: "EMPREGADOS",
+
+    schema: SCHEMA_EMPREGADOS,
+
+    container: "#app",
+
+    stateName: "empregados",
+
+    options: {
+
+        titulo: "Cadastro de Empregados",
+
+        tabela: "Empregados Cadastrados",
+
+        permitirNovo: true,
+
+        permitirEditar: true,
+
+        permitirExcluir: true
+
+    }
+
+});
