7bc5bb3ee08e0fe8ce32617af80312ac86ccdf73
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-22 15:34:43 -0300

    Update crud.js

--- js/engine/crud.js
+++ js/engine/crud.js
@@ -1,6 +1,7 @@
 import {
     listar,
     obter,
+    salvar,
     criar,
     atualizar,
     excluir
@@ -9,27 +10,75 @@ import {
 
 export function createCrud(entity) {
 
+    if (!entity) {
+
+        throw new Error(
+            "CRUD: entity não informada."
+        );
+
+    }
+
+
     return {
 
         listar() {
-            return listar(entity);
+
+            return listar(
+                entity
+            );
+
         },
+
 
         obter(id) {
-            return obter(entity, id);
+
+            return obter(
+                entity,
+                id
+            );
+
         },
+
+
+        salvar(dados) {
+
+            return salvar(
+                entity,
+                dados
+            );
+
+        },
+
 
         criar(dados) {
-            return criar(entity, dados);
+
+            return criar(
+                entity,
+                dados
+            );
+
         },
+
 
         atualizar(dados) {
-            return atualizar(entity, dados);
+
+            return atualizar(
+                entity,
+                dados
+            );
+
         },
 
+
         excluir(id) {
-            return excluir(entity, id);
+
+            return excluir(
+                entity,
+                id
+            );
+
         }
 
     };
+
 }
