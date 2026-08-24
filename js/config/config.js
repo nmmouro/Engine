893f5b296996e98881a57bb9f9d4e6f683c93c1d
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-24 19:45:37 -0300

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
 
     api: {
-        url: "https://script.google.com/macros/s/AKfycbxn-wbUsmOnIDNXujtlIxYiljvAjEOFBcdXu6ydfi-k_lVG-qQpqwN6dAeaTgagADk/exec"
+        url: "https://script.google.com/macros/s/AKfycbxn-wbUsmOnIDNXujtlIxYiljvAjEOFBcdXu6ydfi-k_lVG-qQpqwN6dAeaTgagADk/exec "
     },
 
     ui: {
