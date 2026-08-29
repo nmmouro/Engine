e904a05217251a8cb5da18b21d0a52031ec004fb
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-29 08:01:00 -0300

    Fix syntax error in config.js
    Added a comma after the API URL for proper syntax.

--- js/config/config.js
+++ js/config/config.js
@@ -1,7 +1,9 @@
 const CONFIG = {
 
     api: {
-        url: "https://iomekawabacmuhlltket.supabase.co/rest/v1/"
+        url: "https://iomekawabacmuhlltket.supabase.co/rest/v1/",
+
+       key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvbWVrYXdhYmFjbXVobGx0a2V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MDc3NzQsImV4cCI6MjEwMzQ4Mzc3NH0.j_LBkEyldq5u6-ucqpSmSOC3wM-yjSfQGqwD70OXbPQ"
     },
 
     engine: {
